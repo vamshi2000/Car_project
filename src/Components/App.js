63ea5ab4a1d79d2f1f3a6f4499c577be8e0e8664
vamshi <vamshi.edulakanti@shos.com>
2018-03-07 12:26:50 -0600

    First commit

---
+++ src/Components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../Styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <section id="grid">
+      <div className="container">
+        <div className="grid">
+          <div className="col-1 col-md-6 col-lg-12">
+            Hello
+          </div>
+          <div className="col-1 col-md-6 col-lg-12">
+            Hello
+          </div>
+          <div className="col-1">
+            Hello
+          </div>
+        </div>
+
+        <div className="grid">
+          <div className="col-3">
+            Three on a new line
+          </div>
+        </div>
+      </div>
+    </section>
+    );
+  }
+}
+
+export default App;
